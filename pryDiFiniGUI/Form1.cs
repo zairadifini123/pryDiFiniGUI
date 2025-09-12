054c43d5aa90d4f3bd357de8b876ca1783a633de
zairadifini <zairadifni123@gmail.com>
2025-09-12 12:14:05 -0300

    .

--- pryDiFiniGUI/Form1.cs
+++ pryDiFiniGUI/Form1.cs
@@ -1,8 +1,8 @@
 namespace pryDiFiniGUI
 {
-    public partial class Form1 : Form
+    public partial class frmControles : Form
     {
-        public Form1()
+        public frmControles()
         {
             InitializeComponent();
         }
